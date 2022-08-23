27957f2f6ae400595ec93949a1fbc00f8e9aafa7
Rondez Rayan <rayan.rondez@divtec.ch>
2022-08-23 09:52:24 +0200

    2nd commit reste à tester

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /**
  * Fichier de base pour application web HTML/CSS/JS
- * @author  Steve Fallet
+ * @author  Rayan Rondez
  * @version 0.1
  * @since   2019-08-19
  */
@@ -8,13 +8,13 @@
 (function main() {
   'use strict'; // Demande un interprétation stricte du code
 
-  let celsius = Number (prompt('Temperature en celsius: ${celsius}') );
+  let celsius = Number (prompt('Temperature en celsius:') );
 
   if(isNaN(celsius)){
     alert('Veuillez entrez une temperature valide !');
   }
   else{
-    alert('${celsius} = ${celsius * 9 / 5 + 32}');
+    alert(`${celsius}°C = ${celsius * 9 / 5 + 32}°F`);
   }
 
 }()); // Main IIFE
